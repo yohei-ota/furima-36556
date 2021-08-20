b8e751ec21332e93ae43a36ab816607eb32d6bfc
yohei-ota <freeeedom20xx@gmail.com>
2021-08-20 18:35:02 +0900

    itemモデルのフェイクデータ作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {Faker::Commerce.product_name}
+    text {Faker::Lorem.sentence}
+    category {Category.find(2)}
+    status {Status.find(2)}
+    cost {Cost.find(2)}
+    area {Area.find(2)}
+    day {Day.find(2)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open("public/images/sample1.png"), filename: "sample1.png")
+    end
   end
 end
