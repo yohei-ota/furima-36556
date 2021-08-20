4e5856666fb74b18d156a5e60949e923144dbe25
yohei-ota <freeeedom20xx@gmail.com>
2021-08-20 11:24:11 +0900

    商品出品すると商品情報が保存される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,27 @@ class ItemsController < ApplicationController
   end
 
   def new
-  
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to root_path
+    end
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    prams.require(:item)
+         .permit(:item_name, :text,:price)
+         .merge(:category_id, :status_id, :cost_id, :area_id, :days_id, user_id: current_user.id)
+  end
 end
