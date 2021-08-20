ddca5b51accf6e96df936996e5ce561e25e7831e
yohei-ota <freeeedom20xx@gmail.com>
2021-08-20 22:09:54 +0900

    テストコードのインデント揃え

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
     item_name {Faker::Commerce.product_name}
-    text {Faker::Lorem.sentence}
-    category {Category.find(2)}
-    status {Status.find(2)}
-    cost {Cost.find(2)}
-    area {Area.find(2)}
-    day {Day.find(2)}
-    price {Faker::Number.between(from: 300, to: 9999999)}
+    text      {Faker::Lorem.sentence}
+    category  {Category.find(2)}
+    status    {Status.find(2)}
+    cost      {Cost.find(2)}
+    area      {Area.find(2)}
+    day       {Day.find(2)}
+    price     {Faker::Number.between(from: 300, to: 9999999)}
     association :user
 
     after(:build) do |item|
