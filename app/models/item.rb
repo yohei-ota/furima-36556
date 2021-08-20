f18df911ccd008e997e5649f172568d43252b432
yohei-ota <freeeedom20xx@gmail.com>
2021-08-20 18:34:05 +0900

    priceを整数のみのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,8 @@ class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
   
-  validates :area_id, :category_id, :cost_id, :days_id, :status_id, :item_name, :text, :price, :image, presence: true
-  validates :area_id, :category_id, :cost_id, :days_id, :status_id, numericality: { other_than: 1 , message: "を正しく入力してください"}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :area_id, :category_id, :cost_id, :day_id, :status_id, :item_name, :text, :price, :image, presence: true
+  validates :area_id, :category_id, :cost_id, :day_id, :status_id, numericality: { other_than: 1 , message: "を正しく入力してください"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :price, format: { with: /\A[0-9]+\z/ }
 end
