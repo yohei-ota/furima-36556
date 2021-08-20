d0c1a140dec5b53d627b6198c0de244fd5eb9cba
yohei-ota <freeeedom20xx@gmail.com>
2021-08-20 15:56:54 +0900

    商品金額範囲の制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,6 @@ class Item < ApplicationRecord
   
   validates :area_id, :category_id, :cost_id, :days_id, :status_id, :item_name, :text, :price, :image, presence: true
   validates :area_id, :category_id, :cost_id, :days_id, :status_id, numericality: { other_than: 1 , message: "を正しく入力してください"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
 end
