2380b78d1b2978241dd38c4df618c021bcffbb23
yohei-ota <freeeedom20xx@gmail.com>
2021-08-21 15:31:29 +0900

    未ログインのユーザーor出品者以外のユーザーのアクセス制限の記述を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :redirect_root, only: [:edit, :update, :destroy]
   
   def index
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
 
   def redirect_root
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
